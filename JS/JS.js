08142ccf11e23032a6cf12433dc5faf1fa6e8a64
Paulo Victor <pv_silva@id.uff.br>
2023-11-07 21:56:29 -0300

    modified:   JS/JS.js 	renamed:    Morandi Regular.otf -> MorandiRegular.otf 	modified:   Site.html 	modified:   estilocss.css

--- JS/JS.js
+++ JS/JS.js
@@ -1,1 +1,21 @@
+const containerDeLinks = document.getElementById("containerDeLinks")
 
+const urls = [
+    "https://www.xvideos.com/",
+    "https://discord.gg/SmzHxAdj",
+    "https://www.google.com/intl/pt-BR/gmail/about/"
+
+];
+
+for (var i = 0; i < urls.length; i++) {
+    const botoesDosLinks = document.createElement("botoesDosLinks");
+    botoesDosLinks.className = "botoesDosLinks";
+    botoesDosLinks.textContent = 'botoesDosLinks ${i + 1}';
+
+    botoesDosLinks.addEventListener("click", function() {
+        window.location.href = urls[i];
+
+    });
+
+    gridContainer.appendChild(botoesDosLinks)
+}
